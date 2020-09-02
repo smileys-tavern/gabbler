d5027a1a808b266397defc3410374364ecb74791
Tyler-pierce <tyler.pierce@gmail.com>
2020-09-02 07:12:50 -0700

    Following style direction

--- lib/gabbler_web/views/room_view.ex
+++ lib/gabbler_web/views/room_view.ex
@@ -8,4 +8,14 @@ defmodule GabblerWeb.RoomView do
       String.replace(acc, "%{#{key}}", to_string(value))
     end)
   end
+
+  def post_link(%{name: name}, %{parent_type: "room", hash: hash, title: title}, _) do
+    "/r/#{name}/comments/#{hash}/#{String.replace(title, "#", "")}"
+  end
+
+  def post_link(%{name: name}, %{hash: hash}, %{hash: op_hash}) do
+    "/r/#{name}/comments/#{op_hash}/focus/#{hash}"
+  end
+
+  def post_link(_room, _post, _op), do: "#"
 end
